7f4baa7460e59aeaebf54fded2fd856f96358a47
tir <davide.lanti.@unibz.it>
2014-06-30 13:56:38 +0200

    Begin of support to ORACLE

--- dataPumper/src/main/java/connection/DBMSConnection.java
+++ dataPumper/src/main/java/connection/DBMSConnection.java
@@ -56,32 +56,57 @@ public class DBMSConnection {
 	private static Logger logger = Logger.getLogger(DBMSConnection.class.getCanonicalName());
 	
 	public DBMSConnection(String jdbcConnector, String database, String username, String password){
-				
+		
 		this.jdbcConnector = jdbcConnector;
 		this.databaseUrl = database;
 		this.username = username;
 		this.password = password;
 		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		if( this.jdbcConnector.equals("jdbc:mysql") ){ // Mysql
+			
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+			logger.debug("MySQL JDBC driver loaded ok");
+			
+			String url = 
+					jdbcConnector + "://" + databaseUrl 
+					+ "?useServerPrepStmts=false&rewriteBatchedStatements=true&user=" + username 
+					+ "&password=" + password;
+			try {
+				connection = DriverManager.getConnection(url, username, password);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}		
+			
+			schemas = new HashMap<String, Schema>();
+			fillDatabaseSchemas();
 		}
-		
-		logger.debug("MySQL JDBC driver loaded ok");
-
-		String url = 
-				jdbcConnector + "://" + databaseUrl 
-				+ "?useServerPrepStmts=false&rewriteBatchedStatements=true&user=" + username 
-				+ "&password=" + password;
-		try {
-			connection = DriverManager.getConnection(url, username, password);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}		
-		
-		schemas = new HashMap<String, Schema>();
-		fillDatabaseSchemas();
+		else{ // ORACLE
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+			logger.debug("ORACLE JDBC driver loaded ok");
+			
+//			String url = 
+//					jdbcConnector + "://" + databaseUrl 
+//					+ "?useServerPrepStmts=false&rewriteBatchedStatements=true&user=" + username 
+//					+ "&password=" + password;
+//			try {
+//				connection = DriverManager.getConnection(url, username, password);
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//			}		
+//			
+//			schemas = new HashMap<String, Schema>();
+//			fillDatabaseSchemas();
+		}
 	}
 	
 	public String getJdbcConnector(){
