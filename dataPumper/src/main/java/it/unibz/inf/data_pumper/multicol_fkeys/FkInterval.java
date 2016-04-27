72cc92691d2a2d18b7d1b509b21605180c2ab7c3
tir <davide.lanti@unibz.it>
2016-04-27 16:10:56 +0200

    version

--- dataPumper/src/main/java/it/unibz/inf/data_pumper/multicol_fkeys/FkInterval.java
+++ dataPumper/src/main/java/it/unibz/inf/data_pumper/multicol_fkeys/FkInterval.java
@@ -2,6 +2,6 @@ package it.unibz.inf.data_pumper.multicol_fkeys;
 
 public class FkInterval {
     private Label label;
-    private List<Interval> fkTupleSchema; 
+//    private List<Interval> fkTupleSchema; 
     
 };
