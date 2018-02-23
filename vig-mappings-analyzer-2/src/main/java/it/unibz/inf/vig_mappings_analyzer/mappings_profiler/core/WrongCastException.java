872b80da822d3f4ad7177dd6f1acf08fd0601340
tir <davide.lanti@unibz.it>
2018-02-23 14:40:18 +0100

    rename

--- vig-mappings-analyzer-2/src/main/java/it/unibz/inf/vig_mappings_analyzer/mappings_profiler/core/WrongCastException.java
+++
@@ -1,8 +0,0 @@
-package it.unibz.inf.vig_mappings_analyzer.mappings_profiler.core;
-
-public class WrongCastException extends Exception {
-    private static final long serialVersionUID = 7271631810112312592L;
-    public WrongCastException(String string) {
-	super(string);
-    }
-};
